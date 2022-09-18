9a4db985faacb2dbde1286f8177c1cb55db78ffc
Hoangxt <dinhhuyhoangxt@gmail.com>
2022-09-18 14:16:55 +0700

    Fetch All Users and user's comment

--- store/authStore.ts
+++ store/authStore.ts
@@ -2,8 +2,11 @@ import create from "zustand";
 import { persist } from "zustand/middleware";
 import axios from "axios";
 
+import { BASE_URL } from "../utils";
+
 const authStore = (set: any) => ({
   userProfile: null,
+  allUsers: [],
 
   addUser: (user: any) => {
     set({ userProfile: user });
@@ -11,6 +14,12 @@ const authStore = (set: any) => ({
 
   removeUser: () => {
     set({ userProfile: null });
+  },
+
+  fetchAllUsers: async () => {
+    const response = await axios.get(`${BASE_URL}/api/users`);
+
+    set({ allUsers: response.data });
   },
 });
 
